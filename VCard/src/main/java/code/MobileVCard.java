ae9633c5e72bd3f019243a8fee110911379721ab
Seaborg7 <virus96@tlen.pl>
2020-01-15 00:07:44 +0100

    Commit

---
+++ VCard/src/main/java/code/MobileVCard.java
@@ -0,0 +1,13 @@
+package code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileVCard {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileVCard.class, args);
+    }
+
+}
